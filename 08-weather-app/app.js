95febd7bf296a9f101e0fad903374a6762d1cab6
Sebastian F <sebpfdev@gmail.com>
2022-01-04 22:05:23 -0500

    Format weather data for display

--- 08-weather-app/app.js
+++ 08-weather-app/app.js
@@ -1,10 +1,10 @@
 // Markup Data
 const iconNameToSizeMap = {
+  'partly-cloudy': { width: 230, height: 209 },
   cloudy: { width: 264, height: 166 },
   sunny: { width: 208, height: 213 },
   stormy: { width: 246, height: 187 },
   snowy: { width: 230, height: 196 },
-  'partly-cloudy': { width: 230, height: 209 },
   rainy: { width: 160, height: 222 },
 };
 
@@ -56,15 +56,6 @@ function getLocation() {
   }
 }
 
-async function testGetLocationWeather() {
-  const position = await getLocation();
-  console.log(position);
-
-  const weather = await getWeather(position);
-  console.log(weather);
-}
-testGetLocationWeather();
-
 // Weather Services
 async function getWeather(position) {
   const openWeatherKey = '0bb338e53966913f3a5d9c70366f0e35'; // Fill in: https://openweathermap.org/
@@ -86,3 +77,69 @@ async function getWeather(position) {
 
   return await openWeatherRes.json();
 }
+
+function weatherCondition(weatherId) {
+  let condition;
+  if (weatherId < 300) {
+    condition = 'stormy';
+  } else if (weatherId < 600) {
+    condition = 'rainy';
+  } else if (weatherId < 700) {
+    condition = 'snowy';
+  } else if (weatherId <= 800) {
+    condition = 'sunny';
+  } else if (weatherId <= 803) {
+    condition = 'partly-cloudy';
+  } else {
+    condition = 'cloudy';
+  }
+
+  return condition;
+}
+
+// AlpineJS services
+document.addEventListener('alpine:init', () => {
+  Alpine.data('weather', () => ({
+    weekWeather: [],
+
+    init() {
+      this.getLocationWeather();
+    },
+
+    async getLocationWeather() {
+      const position = await getLocation();
+      const weather = await getWeather(position);
+      console.log(weather.daily);
+      this.formatWeatherData(weather.daily);
+    },
+
+    formatWeatherData(weatherDaily) {
+      for (let day = 0; day < 7; day++) {
+        const dayData = weatherDaily[day];
+        const dayDate = new Date(dayData.dt * 1000);
+        const condition = weatherCondition(dayData.weather[0].id);
+
+        const tempData = {
+          day: daysOfWeekMap[dayDate.getDay()],
+          date: dayDate.getDate(),
+          condition,
+          conditionIcon: iconNameToSizeMap[condition],
+          tempHigh: dayData.temp.day,
+          tempLow: dayData.temp.night,
+          precipitation: dayData.pop * 100,
+        };
+        // console.log(`tempData: ${JSON.stringify(tempData)}`);
+
+        this.weekWeather.push(tempData);
+      }
+    },
+  }));
+});
+
+/**
+ * Get location permission
+ * Get location (async)
+ * Get date
+ * Get weather data (async)
+ * Display weather data
+ */
