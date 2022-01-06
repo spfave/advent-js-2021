2fb79910d89474f0b02e6f0a76b87632f5f290a2
Sebastian F <sebpfave2@gmail.com>
2022-01-05 22:18:49 -0500

    Refactor functionality to Alpine data

--- 09-photo-carousel/app.js
+++ 09-photo-carousel/app.js
@@ -1,3 +1,21 @@
 document.addEventListener('alpine:init', () => {
   Alpine.store('images', { content: images });
+
+  Alpine.data('photoCarousel', () => ({
+    imageId: 0,
+
+    setImage(el) {},
+
+    imageName(imageId) {
+      return this.$store.images.content[imageId].image;
+    },
+    imageTxt(imageId) {
+      return this.$store.images.content[imageId].caption;
+    },
+
+    imagePath(imageId) {
+      const imageName = this.imageName(imageId);
+      return `./images/${imageName}`;
+    },
+  }));
 });
