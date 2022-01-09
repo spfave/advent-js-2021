089836ac5d82a12be0c9b796150f8d31ce521533
Sebastian F <sebpfave2@gmail.com>
2022-01-08 23:24:35 -0500

    Refactor cart to template render

--- 02-ecommerce-component/app.js
+++ 02-ecommerce-component/app.js
@@ -10,7 +10,22 @@ document.addEventListener('alpine:init', () => {
   // Store
   Alpine.store('menu', {
     items: [],
-    cart: [],
+    cart: [
+      {
+        name: 'French Fries with Ketchup',
+        price: 2.23,
+        image: 'plate__french-fries.png',
+        alt: 'French Fries',
+        count: 2,
+      },
+      {
+        name: 'Salmon and Vegetables',
+        price: 5.12,
+        image: 'plate__salmon-vegetables.png',
+        alt: 'Salmon and Vegetables',
+        count: 1,
+      },
+    ],
 
     init() {
       this.items = menuItems;
@@ -20,4 +35,13 @@ document.addEventListener('alpine:init', () => {
       return dollarFormatter.format(value);
     },
   });
+
+  // Data
+  Alpine.data('menu', () => ({}));
+
+  Alpine.data('cart', () => ({
+    get cartEmpty() {
+      return this.$store.menu.cart.length === 0;
+    },
+  }));
 });
