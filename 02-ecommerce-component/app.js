ca56a5778360ffbc25d25faeefb836dabac466f9
Sebastian F <sebpfave2@gmail.com>
2022-01-08 23:17:19 -0500

    Create display dollar function

--- 02-ecommerce-component/app.js
+++ 02-ecommerce-component/app.js
@@ -1,10 +1,23 @@
+// Cash formatter to
+const dollarFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+});
+
+// Alpine JS
 document.addEventListener('alpine:init', () => {
+  // Store
   Alpine.store('menu', {
     items: [],
     cart: [],
 
     init() {
       this.items = menuItems;
+    },
+
+    displayDollar(value) {
+      return dollarFormatter.format(value);
     },
   });
 });
