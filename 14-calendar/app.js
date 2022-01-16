d91c657f72fec028df9b21d30d1c930541e4f31a
Sebastian F <sebpfdev@gmail.com>
2022-01-16 16:39:46 -0500

    Render month dates

--- 14-calendar/app.js
+++ 14-calendar/app.js
@@ -4,12 +4,34 @@ document.addEventListener('alpine:init', () => {
   Alpine.data('calendar', () => ({
     today: new Date(),
     date: new Date(),
+    monthDates: null,
+
+    init() {
+      this.monthDays();
+    },
 
     monthHeader() {
       const year = this.date.getFullYear();
       const month = this.date.toLocaleString('default', { month: 'long' });
 
       return `${year} - ${month}`;
+    },
+
+    monthDays() {
+      const year = this.date.getFullYear();
+      const month = this.date.getMonth();
+
+      // determine starting day of month
+      const startDay = new Date(year, month, 1).getDay();
+
+      // create array of leading null dates as needed
+      const leadingDates = [...Array(startDay).fill(null)];
+
+      // create array of month dates
+      const numMonthDays = this.daysInMonth(month + 1, year);
+      const dates = [...Array(numMonthDays).keys()].map((day) => day + 1);
+
+      this.monthDates = leadingDates.concat(dates);
     },
 
     changeMonth(direction) {
@@ -22,11 +44,13 @@ document.addEventListener('alpine:init', () => {
         newMonth.getMonth() === this.today.getMonth()
           ? new Date(this.today)
           : newMonth;
+
+      this.monthDays();
     },
 
     /** Return number of days in month. Use 1 for January, 2 for February, etc. */
-    // daysInMonth(month, year) {
-    //   return new Date(year, month, 0).getDate();
-    // },
+    daysInMonth(month, year) {
+      return new Date(year, month, 0).getDate();
+    },
   }));
 });
