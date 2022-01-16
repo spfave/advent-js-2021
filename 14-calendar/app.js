d4791a9051f24b8dd62651d8f2ed75f23ea7061a
Sebastian F <sebpfdev@gmail.com>
2022-01-16 00:35:20 -0500

    Enable cycle mont next or previous

--- 14-calendar/app.js
+++ 14-calendar/app.js
@@ -2,6 +2,7 @@ const months = [];
 
 document.addEventListener('alpine:init', () => {
   Alpine.data('calendar', () => ({
+    today: new Date(),
     date: new Date(),
 
     monthHeader() {
@@ -10,5 +11,22 @@ document.addEventListener('alpine:init', () => {
 
       return `${year} - ${month}`;
     },
+
+    changeMonth(direction) {
+      const newMonth = new Date(
+        this.date.setMonth(this.date.getMonth() + direction, 1)
+      );
+
+      this.date =
+        newMonth.getFullYear() === this.today.getFullYear() &&
+        newMonth.getMonth() === this.today.getMonth()
+          ? new Date(this.today)
+          : newMonth;
+    },
+
+    /** Return number of days in month. Use 1 for January, 2 for February, etc. */
+    // daysInMonth(month, year) {
+    //   return new Date(year, month, 0).getDate();
+    // },
   }));
 });
