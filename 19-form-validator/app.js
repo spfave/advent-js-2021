5f6613222ec2cf2816ff3c56f56df475ca5025e4
Sebastian F <sebpfdev@gmail.com>
2022-01-17 22:26:34 -0500

    Create Alpine data contexts for name & password

--- 19-form-validator/app.js
+++ 19-form-validator/app.js
@@ -1,11 +1,37 @@
 document.addEventListener('alpine:init', () => {
-  Alpine.data('validator', () => ({
+  Alpine.data('nameValidator', () => ({
     name: '',
-    email: '',
-    password: '',
-    passwordC: '',
+    valid: false,
+    invalid: false,
+    // email: '',
+    // password: '',
+    // passwordC: '',
 
-    validate() {},
+    validateName() {
+      if (validator.isAlpha(this.name)) {
+        this.valid = true;
+        this.invalid = false;
+      } else {
+        this.valid = false;
+        this.invalid = true;
+      }
+    },
+  }));
+
+  Alpine.data('emailValidator', () => ({
+    email: '',
+    valid: false,
+    invalid: false,
+
+    validateEmail() {
+      if (validator.isEmail(this.email)) {
+        this.valid = true;
+        this.invalid = false;
+      } else {
+        this.valid = false;
+        this.invalid = true;
+      }
+    },
   }));
 
 });
