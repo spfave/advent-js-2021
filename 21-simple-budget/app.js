f2249e9872bdb4d021ac5de97e90a9c4741559a4
Sebastian F <sebpfdev@gmail.com>
2022-01-19 22:26:10 -0500

    Setup AlpineJS

---
+++ 21-simple-budget/app.js
@@ -0,0 +1,30 @@
+// Cash formatter
+const dollarFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+});
+
+// Alpine JS
+document.addEventListener('alpine:init', () => {
+  Alpine.data('budget', () => ({
+    income: 0,
+    spent: 0,
+    balance: 0,
+    expenses: [],
+
+    init() {},
+
+    setBudget() {},
+
+    addExpense() {},
+
+    calcSpent() {},
+
+    calcBalance() {},
+
+    displayDollar(value) {
+      return dollarFormatter.format(value);
+    },
+  }));
+});
