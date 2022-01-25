a2885581fc14e4c5c149919557388a3f94c76ac6
Sebastian F <sebpfdev@gmail.com>
2022-01-24 20:10:43 -0500

    Update credit card type displayed

---
+++ 22-credit-card/app.js
@@ -0,0 +1,34 @@
+document.addEventListener('alpine:init', () => {
+  Alpine.data('creditCard', () => ({
+    card: {
+      type: 'visa',
+      number: '',
+      holder: '',
+      expiration: { month: '', year: '' },
+      cvv: '',
+    },
+
+    displayCardType() {
+      const ccn = this.card.number;
+      if (ccn.startsWith('34') || ccn.startsWith('37')) {
+        this.card.type = 'american';
+      } else if (ccn.startsWith('4')) {
+        this.card.type = 'visa';
+      } else if (ccn.startsWith('5')) {
+        this.card.type = 'mastercard';
+      } else if (ccn.startsWith('6')) {
+        this.card.type = 'discover';
+      } else {
+        alert('Enter a valid credit card number');
+      }
+    },
+
+    // displayCardNumber() {
+    //   const ccn = this.card.number;
+    //   return `${ccn.slice(0, 4)} ${ccn.slice(4, 9)}
+    //     ${ccn.slice(9, 13)} ${ccn.slice(13)}`;
+    // },
+
+    enterCVV() {},
+  }));
+});
