0665081722f44c3b3abb4ff981c8bd981dc4f54f
Sebastian F <sebpfdev@gmail.com>
2022-01-25 21:33:53 -0500

    Display toaster after 15 seconds

--- 23-toaster/app.js
+++ 23-toaster/app.js
@@ -1,5 +1,11 @@
 document.addEventListener('alpine:init', () => {
   Alpine.data('toaster', () => ({
     hide: true,
+
+    init() {
+      setTimeout(() => {
+        this.hide = false;
+      }, 15000);
+    },
   }));
 });
